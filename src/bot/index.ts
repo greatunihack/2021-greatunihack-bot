8d46deb290199f8379da1922f764b3773a80b935
Sam Hirst <SamuelJHirst@outlook.com>
2021-09-01 21:22:25 +0100

    create function to prepare discord server for event

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,13 +1,59 @@
-import { Client, Intents } from 'discord.js';
+import { Client, GuildStickerManager, Intents } from 'discord.js';
 
-export default async function Bot() {
-    const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+export default class Bot {
+    client: Client;
 
-    client.on('ready', () => {
-        console.log('Discord Bot Online');
-    });
+    async login() {
+        const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-    await client.login(process.env.DISCORD_BOT_TOKEN);
+        client.on('ready', () => {
+            console.log('Discord Bot Online');
+        });
 
-    return client;
-};
+        await client.login(process.env.DISCORD_BOT_TOKEN);
+
+        this.client = client;
+    }
+
+    async setup(server: string) {
+        const guilds = await this.client.guilds.fetch();
+        const guildManager = guilds.find(guild => guild.id == server);
+        
+        if (!guildManager) return 404;
+        
+        const guild = await guildManager.fetch();
+        const roles = await guild.roles.fetch();
+        const channels = await guild.channels.fetch();
+        
+        roles.each(async (role) => {
+            if (!role.managed && role.name != '@everyone') await role.delete();
+        });
+        channels.each(async (channel) => {
+            await channel.delete();
+        });
+
+        const everyoneRole = guild.roles.everyone;
+        const staffRole = await guild.roles.create({ name: "Staff", color: '#01ACFF', hoist: true, permissions: ["ADMINISTRATOR"], mentionable: false });
+        const sponsorRole = await guild.roles.create({ name: "Sponsor", color: '#EB403B', hoist: true, mentionable: false });
+        const judgeRole = await guild.roles.create({ name: "Judge", color: '#32C744', hoist: true, mentionable: false });
+        const mentorRole = await guild.roles.create({ name: "Mentor", color: '#765CB1', hoist: true, mentionable: false });
+
+        const announcementsCategory = await guild.channels.create("Group Channels", { type: "GUILD_CATEGORY" });
+        const welcomeChannel = await guild.channels.create("welcome", { parent: announcementsCategory, permissionOverwrites: [{ id: staffRole, allow: ['SEND_MESSAGES'] }, { id: everyoneRole, deny: ['SEND_MESSAGES', 'EMBED_LINKS'] }] });
+        const announcementsChannel = await guild.channels.create("announcements", { parent: announcementsCategory, permissionOverwrites: [{ id: staffRole, allow: ['SEND_MESSAGES'] }, { id: everyoneRole, deny: ['SEND_MESSAGES', 'EMBED_LINKS'] }] });
+        const sponsorsAnnouncementsChannel = await guild.channels.create("sponsors", { parent: announcementsCategory, permissionOverwrites: [{ id: staffRole, allow: ['SEND_MESSAGES'] }, { id: sponsorRole, allow: ['SEND_MESSAGES', 'EMBED_LINKS'] }, { id: everyoneRole, deny: ['SEND_MESSAGES', 'EMBED_LINKS'] }] });
+        const helpChannel = await guild.channels.create("help", { parent: announcementsCategory, permissionOverwrites: [{ id: everyoneRole, deny: ['EMBED_LINKS'] }] });
+
+        const staffCategory = await guild.channels.create("Staff Channels", { type: "GUILD_CATEGORY" });
+        const staffChannel = await guild.channels.create("staff", { parent: staffCategory, permissionOverwrites: [{ id: staffRole, allow: ['VIEW_CHANNEL'] }, { id: everyoneRole, deny: ['VIEW_CHANNEL'] }] });
+        const sponsorsChannel = await guild.channels.create("sponsors", { parent: staffCategory, permissionOverwrites: [{ id: staffRole, allow: ['VIEW_CHANNEL'] }, { id: sponsorRole, allow: ['VIEW_CHANNEL'] }, { id: everyoneRole, deny: ['VIEW_CHANNEL'] }] });
+        const judgesChannel = await guild.channels.create("judges", { parent: staffCategory, permissionOverwrites: [{ id: staffRole, allow: ['VIEW_CHANNEL'] }, { id: judgeRole, allow: ['VIEW_CHANNEL'] }, { id: everyoneRole, deny: ['VIEW_CHANNEL'] }] });
+        const mentorsChannel = await guild.channels.create("mentors", { parent: staffCategory, permissionOverwrites: [{ id: staffRole, allow: ['VIEW_CHANNEL'] }, { id: mentorRole, allow: ['VIEW_CHANNEL'] }, { id: everyoneRole, deny: ['VIEW_CHANNEL'] }] });
+
+        const teamCategory = await guild.channels.create("Team Channels", { type: "GUILD_CATEGORY" });
+
+        everyoneRole.permissions.remove(['CREATE_INSTANT_INVITE', 'MENTION_EVERYONE']);
+
+        return 200;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,17 @@ dotenv.config();
 const app = express();
 const port = 8080;
 
-const client = Bot();
+const client = new Bot();
+client.login();
 app.set('client', client);
 
 app.get('/', (req: Request, res: Response) => {
     res.sendStatus(200);
+});
+
+app.post('/setup/:server', async (req: Request, res: Response) => {
+    const resp = await client.setup(req.params.server);
+    res.sendStatus(resp);
 });
 
 app.listen(port, () => {
